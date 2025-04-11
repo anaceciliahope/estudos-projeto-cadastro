ea259a1095a405b68b23b7d7cf06b0e13666dcdd
anaceciliahope <anaceciliahope@gmail.com>
2025-04-11 10:42:31 -0300

    Initial commit

---
+++ src/main/java/com/refazendotodo/estudos/EstudosRefazendoToDoApplication.java
@@ -0,0 +1,13 @@
+package com.refazendotodo.estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosRefazendoToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosRefazendoToDoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/refazendotodo/estudos/EstudosRefazendoToDoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.refazendotodo.estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosRefazendoToDoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
